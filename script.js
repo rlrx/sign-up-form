77fecc0c55444c07e8d99931495a21867fbc06d7
rlrx <ryan.lee.ruixiang@gmail.com>
2023-09-23 18:10:40 +0800

    Implemented password checker JS script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // write function to check if password fields match
 function checkPasswordMatch() {
     // get values of the password field
-    let password1 = document.getElementById(".password1").value;
-    let password2 = document.getElementById(".password2").value;
+    let password1 = document.querySelector(".password1").value;
+    let password2 = document.querySelector(".password2").value;
 
     if(password1 == password2) {
-        let pw1 = document.getElementById(".password1");
-        let pw2 = document.getElementById(".password2");
+        let pw1 = document.querySelector(".password1");
+        let pw2 = document.querySelector(".password2");
 
         // remove the error class from both password input fields
         pw1.classList.remove("error");
